717bc05801c3c97323d8450dcfda02ab603bb488
shintech-asp <sheenrusselcastillo@gmail.com>
2025-09-08 17:04:36 +0800

    First commit

---
+++ burbodek/Controllers/IndexController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace burbodek.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult SignUpClient()
+        {
+            return View();
+        }
+        public IActionResult SignUpEmployer()
+        {
+            return View();
+        }
+    }
+}
