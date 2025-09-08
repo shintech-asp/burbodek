cc67b5546af317dff76d5b75fa8051e910d8c1dd
shintech-asp <sheenrusselcastillo@gmail.com>
2025-09-08 21:26:16 +0800

    Done layouts, Messages for Employee and Employer. Job viewing for employee, Job details

---
+++ burbodek/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace burbodek.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult JobInfo()
+        {
+            return View();
+        }
+        public IActionResult Message()
+        {
+            return View();
+        }
+        public IActionResult Compose()
+        {
+            return View();
+        }
+        public IActionResult Reply()
+        {
+            return View();
+        }
+    }
+}
---
+++ burbodek/Controllers/EmployerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace burbodek.Controllers
+{
+    public class EmployerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Message()
+        {
+            return View();
+        }
+        public IActionResult Compose()
+        {
+            return View();
+        }
+        public IActionResult Reply()
+        {
+            return View();
+        }
+    }
+}
