aaf24abaaf6f750e1e1faa790e5cc3c1a0c999a6
shintech-asp <sheenrusselcastillo@gmail.com>
2025-09-08 12:54:46 +0800

    Add project files.

---
+++ burbodek/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using burbodek.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace burbodek.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ burbodek/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace burbodek.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
